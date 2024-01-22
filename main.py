198fb644359a98fe43b61e1c3d5621c96d1d7989
ROARenan <renan.santana.ferreira.1@gmail.com>
2024-01-22 11:30:10 -0300

    feat: Primeira versao de producao

--- main.py
+++ main.py
@@ -1,27 +1,78 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.service import Service
-import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from unicodedata import normalize
+from datetime import datetime
+import requests
+import json
 import time
 
+# Buscando a Grade
+
+def pegar_grade(ra: int, semana: int):
+    now = datetime.now()
+    tables = pd.read_html(f'https://minhagrade-ufabc.up.railway.app/?ra={ra}')
+    #print(f'Total tables: {len(tables)}')
+
+    #Semana 1
+    semana_1 = tables[0]
+
+    #Semana 2
+    semana_2 = tables[1]
+
+    semana_1
+    #segunda = semana_1["Seg"]
+
+    days = {
+        "Seg":"Monday",
+        "Ter":"Tuesday",
+        "Qua":"Wednesday",
+        "Qui":"Thursday",
+        "Sex":"Friday"
+    }
+
+    semana_1.rename(columns=days,inplace=True)
+    semana_2.rename(columns=days,inplace=True)
+
+    formatted_date_time = now.strftime('%A')
+    dia = str(formatted_date_time)
+
+    if semana == 1 or semana == 2:
+        if semana == 1:
+            return str(semana_1[dia][0])+"\n"+str(semana_1[dia][1])
+        if semana == 2:
+            return str(semana_2[dia][0])+"\n"+str(semana_2[dia][1])
+    else:
+        return "Semana fora do range 1 || 2"
+
+
+# Buscar no bot do telegram as mensagens que chegam
+    
 while True:
-    
-    #Lendo RA do caboclo
-    ra = int(input("Qual seria seu RA?\n"))
 
-    #Parar programa se RA = 0
-    if ra == 0:
-        break
-    
-    #geckodriver_path = os.path.abspath("geckodriver")
-    #Mostrar para a Kitty jeito mais bonitinho
-    url = f"https://minhagrade-ufabc.up.railway.app/?ra={ra}"
-    service = Service(executable_path="geckodriver")
-    options = webdriver.FirefoxOptions()
+    key = '6843390780:AAF1ujROv9KLpKUFZEaJ14D9OH8hc6GdnJ0'
+    url = f'https://api.telegram.org/bot{key}/getUpdates'
+    response = requests.request("GET", url= url)
 
-    driver = webdriver.Firefox(service= service, options= options)
+    data = json.loads(response.text)
+    data["result"][0]["message"]["text"]
+
+    data = json.loads(response.text)
+
+    for mensagem in data["result"]:
+        aulas_do_dia = pegar_grade(ra= mensagem["message"]["text"], semana= 1)
+        
+        url = f'https://api.telegram.org/bot{key}/sendMessage'
+
+        params = {
+            "chat_id": mensagem["message"]["chat"]["id"],
+            "text": aulas_do_dia
+        }
+
+        response = requests.request("GET", url= url,params=params)
+
+        data = json.loads(response.text)
+        if data["ok"] == True:
+            print("Mensagem Enviada =)")
     
-    driver.get(url)
-    time.sleep(5)
-    driver.quit()
+    time.sleep(3)
